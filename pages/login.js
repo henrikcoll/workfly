25385e2cd41437dde076edf8790dbf5fd5fd875e
Henrik Næss <henrik@heka.no>
2020-11-23 13:51:08 +0100

    Changed class to className

--- pages/login.js
+++ pages/login.js
@@ -9,33 +9,33 @@ export default function Login({ csrfToken }) {
         <title>Task Manager - Login</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 dark:bg-gray-800">
-        <div class="max-w-md w-full space-y-8">
-          <div class="w-full max-w-xs">
-            <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 dark:bg-gray-700" method="post" action="/api/auth/callback/credentials">
+      <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 dark:bg-gray-800">
+        <div className="max-w-md w-full space-y-8">
+          <div className="w-full max-w-xs">
+            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 dark:bg-gray-700" method="post" action="/api/auth/callback/credentials">
               <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-              <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2 dark:text-gray-300" for="email">
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-gray-300" for="email">
                   Email
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline dark:bg-gray-600 dark:border-gray-700 dark:text-gray-300" id="email" name="email" type="emails" placeholder="Email" />
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline dark:bg-gray-600 dark:border-gray-700 dark:text-gray-300" id="email" name="email" type="emails" placeholder="Email" />
               </div>
-              <div class="mb-6">
-                <label class="block text-gray-700 text-sm font-bold mb-2 dark:text-gray-300" for="password">
+              <div className="mb-6">
+                <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-gray-300" for="password">
                   Password
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline dark:bg-gray-600 dark:border-gray-700 dark:text-gray-300" id="password" name="password" type="password" placeholder="**" />
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline dark:bg-gray-600 dark:border-gray-700 dark:text-gray-300" id="password" name="password" type="password" placeholder="**" />
               </div>
-              <div class="flex items-center justify-between">
-                <input type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" value="Sign in"></input>
+              <div className="flex items-center justify-between">
+                <input type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" value="Sign in"></input>
                 <Link href="/register">
-                  <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800 dark:hover:text-blue-600" href="#">
+                  <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800 dark:hover:text-blue-600" href="#">
                     Register
                   </a>
                 </Link>
               </div>
             </form>
-            <p class="text-center text-gray-500 text-xs">
+            <p className="text-center text-gray-500 text-xs">
               &copy;2020 Workfly. All rights reserved.
             </p>
           </div>
--- pages/register.js
+++ pages/register.js
@@ -30,32 +30,32 @@ export default function Register() {
         <title>Task Manager - Login</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 dark:bg-gray-800">
-        <div class="max-w-md w-full space-y-8">
-          <div class="w-full max-w-xs">
-            <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 dark:bg-gray-700" method="post" action="/api/auth/callback/credentials">
-              <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2 dark:text-gray-300" for="name">
+      <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 dark:bg-gray-800">
+        <div className="max-w-md w-full space-y-8">
+          <div className="w-full max-w-xs">
+            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 dark:bg-gray-700" method="post" action="/api/auth/callback/credentials">
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-gray-300" for="name">
                   Name
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline dark:bg-gray-600 dark:border-gray-700 dark:text-gray-300" id="name" name="name" type="name" placeholder="Name" ref={nameRef} />
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline dark:bg-gray-600 dark:border-gray-700 dark:text-gray-300" id="name" name="name" type="name" placeholder="Name" ref={nameRef} />
               </div>
-              <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2 dark:text-gray-300" for="email">
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-gray-300" for="email">
                   Email
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline dark:bg-gray-600 dark:border-gray-700 dark:text-gray-300" id="email" name="email" type="email" placeholder="Email" ref={emailRef} />
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline dark:bg-gray-600 dark:border-gray-700 dark:text-gray-300" id="email" name="email" type="email" placeholder="Email" ref={emailRef} />
               </div>
-              <div class="mb-6">
-                <label class="block text-gray-700 text-sm font-bold mb-2 dark:text-gray-300" for="password">
+              <div className="mb-6">
+                <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-gray-300" for="password">
                   Password
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline dark:bg-gray-600 dark:border-gray-700 dark:text-gray-300" id="password" name="password" type="password" placeholder="**" ref={passRef} />
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline dark:bg-gray-600 dark:border-gray-700 dark:text-gray-300" id="password" name="password" type="password" placeholder="**" ref={passRef} />
               </div>
-              <div class="flex items-center justify-between">
-                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={doRegister}>Sign up</button>
+              <div className="flex items-center justify-between">
+                <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={doRegister}>Sign up</button>
                 <Link href="/login">
-                  <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800 dark:hover:text-blue-600" href="#">
+                  <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800 dark:hover:text-blue-600" href="#">
                     Login
                   </a>
                 </Link>
